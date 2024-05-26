b8b29577efa6d0eb221caf0e328dbd80da10cd0f
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2024-05-26 17:40:11 -0500

    Creación de módulo de prueba de Log

---
+++ PersistenciaTransaccional/src/application/Log4jExample.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4jExample {
+    private static final Logger logger = LogManager.getLogger(Log4jExample.class);
+
+    public static void main(String[] args) {
+        logger.info("Mensaje de información");
+        logger.error("Mensaje de error");
+    }
+}
